6208b831d0e43c6f2d4640d41a1cdf76efce6954
NIkola-mkd <58082595+NIkola-mkd@users.noreply.github.com>
2020-05-03 22:37:18 +0300

    Add files via upload

---
+++ Многопоточное программирование/Многопоточность_3.cpp
@@ -0,0 +1,35 @@
+﻿/*Возврат результата из потока по ссылке*/
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+
+void doWork(int &a) {
+
+    this_thread::sleep_for(chrono::milliseconds(1000));
+    cout << "ID:"<<this_thread::get_id()<<"\t" << "doWork started\t====" << endl;
+    this_thread::sleep_for(chrono::milliseconds(5000));
+    a *= 2;
+    cout << "ID:" << this_thread::get_id() << "\t" << "doWork stopped\t====" << endl;
+}
+
+
+int main()
+{
+    int q = 5;
+    thread t(doWork,std::ref(q));
+   // doWork(q);
+   
+    
+  
+    for (size_t i = 0;i<10;i++) {
+        cout << "ID:" << this_thread::get_id() << "\t main works" << i << endl;
+        this_thread::sleep_for(chrono::milliseconds(500));
+    }
+    t.join();
+    cout << q << endl;
+    return 0;
+}
